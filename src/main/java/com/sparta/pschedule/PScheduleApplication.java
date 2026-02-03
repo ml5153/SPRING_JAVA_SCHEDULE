3e6584dc941e8582dbf598258bf0aac738eda3f3
yspark <youngsue5153@gmail.com>
2026-02-03 16:17:42 +0900

    init: Schedule Project Kick-Off

---
+++ src/main/java/com/sparta/pschedule/PScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.pschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PScheduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PScheduleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/pschedule/PScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.pschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PScheduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
