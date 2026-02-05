7474f01f11255464f9f72cc0a65702cf4aaa0985
yspark <youngsue5153@gmail.com>
2026-02-05 12:20:52 +0900

    refac: 쿼리메서드 주석 추가

--- src/main/java/com/sparta/pschedule/repository/CommentRepository.java
+++ src/main/java/com/sparta/pschedule/repository/CommentRepository.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+// 쿼리가 길어지면... 이걸 함수규격을 어떻게 맞춰야하지? 더 복잡해지지않을까요? => 찾아보니 쿼리 DSL 있는것같다?
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
